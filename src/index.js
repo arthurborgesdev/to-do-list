f2bcad22d9f8bbf6ea7886775fc2d8b6c01483ee
Arthur Borges <arthurborges.dev@gmail.com>
2021-07-13 17:52:11 -0300

    Populate the todo lists on the page, following the index order

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const todo = [
+  {
+    description: "Finish watching S.H.I.E.L.D season 2",
+    completed: false,
+    index: 0
+  },
+  {
+    description: "Win the Indigo League in Pokémon Yellow",
+    completed: false,
+    index: 1
+  },
+  {
+    description: "Finish the course about NodeJS in Udemy",
+    completed: false,
+    index: 2
+  }
+]
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+const populateTodos = () => {
+  const sortedTodo = todo.sort((a, b) => a.index - b.index)
+  
+  for(let i = 0; i < sortedTodo.length; i += 1) {
+    document.getElementById('todo-list').insertAdjacentHTML('beforeend',`
+      <div>
+        <div>
+          <input type="checkbox" name="item-${sortedTodo[i].index}">
+          <label for="item-${sortedTodo[i].index}">${sortedTodo[i].description}</label>
+        </div>
+        <i class="fas fa-ellipsis-v"></i> 
+      </div>
+    `)
+  }
 }
 
-document.body.appendChild(component());
+window.onload = populateTodos()
